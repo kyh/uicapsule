30d079938803bb1fb356bbed2320183d34211c0c
Kaiyu Hsu <im.kaiyu@gmail.com>
2021-02-01 18:00:32 -0800

    fix router errors

--- src/components/SettingsNav.js
+++ src/components/SettingsNav.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { useRouter } from "next/router";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import Link from "next/link";
 
 function SettingsNav(props) {
+  const router = useRouter();
   return (
     <Tabs
       value={props.activeKey}
@@ -11,15 +12,21 @@ function SettingsNav(props) {
       textColor="primary"
       centered
     >
-      <Link href="/settings/general" passHref>
-        <Tab label="General" value="general" component="a" />
-      </Link>
-      <Link href="/settings/password" passHref>
-        <Tab label="Password" value="password" component="a" />
-      </Link>
-      <Link href="/settings/billing" passHref>
-        <Tab label="Billing" value="billing" component="a" />
-      </Link>
+      <Tab
+        label="General"
+        value="general"
+        onClick={() => router.push("/settings/general")}
+      />
+      <Tab
+        label="Password"
+        value="password"
+        onClick={() => router.push("/settings/password")}
+      />
+      <Tab
+        label="Billing"
+        value="billing"
+        onClick={() => router.push("/settings/billing")}
+      />
     </Tabs>
   );
 }
