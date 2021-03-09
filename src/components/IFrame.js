7fa1771df414e19dbc06b7febaaab3693d6b2b28
Kaiyu Hsu <im.kaiyu@gmail.com>
2021-03-08 18:42:23 -0800

    sandbox iframe

--- src/components/IFrame.js
+++ src/components/IFrame.js
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 
-const IFrame = styled.iframe`
+const StyledIFrame = styled.iframe`
   display: block;
   width: 100%;
   height: 100%;
   border: none;
 `;
+
+const IFrame = (props) => {
+  return <StyledIFrame sandbox="" {...props} />;
+};
 
 export default IFrame;
