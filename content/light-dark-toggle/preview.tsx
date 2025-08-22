0468a5e5140499be702afe9bd7f00e1138da6d2b
Kaiyu Hsu <im.kaiyu@gmail.com>
2025-08-22 00:00:12 -0700

    add theme toggle

--- content/light-dark-toggle/preview.tsx
+++ content/light-dark-toggle/preview.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { LightDarkToggle } from "./source";
 
 const Preview = () => {
-  return <LightDarkToggle />;
+  const [isLight, setIsLight] = useState(true);
+
+  return (
+    <div
+      className={`main flex h-dvh items-center justify-center ${isLight ? "bg-stone-300" : "bg-stone-900"}`}
+    >
+      <button
+        className={`grid size-12 place-items-center rounded-xl bg-gradient-to-t shadow-lg ${isLight ? "from-[#f8fafc] to-[#f1f5f9] text-stone-950" : "from-[#020617] to-[#0F172A] text-stone-50"}`}
+        onClick={() => setIsLight(!isLight)}
+      >
+        <LightDarkToggle isLight={isLight} />
+      </button>
+    </div>
+  );
 };
 
 export default Preview;
