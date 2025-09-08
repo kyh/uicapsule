ecd2c1d8c37afa1bb38729edfe4210ad262ff465
Kaiyu Hsu <im.kaiyu@gmail.com>
2025-09-07 21:05:40 -0700

    update tailwind

--- apps/nextjs/src/app/(content)/ui/[slug]/page.tsx
+++ apps/nextjs/src/app/(content)/ui/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { ResponsiveAside } from "@/app/(content)/_components/aside";
 import { ContentRenderer } from "@/app/(content)/_components/content-renderer";
-import { SandpackProvider } from "@/app/(content)/_components/sandpack";
+import { ContentProvider } from "@/app/(content)/_components/sandpack";
 import { getContentComponent } from "@/lib/content";
 
 type Props = {
@@ -14,41 +14,13 @@ const Page = async ({ params }: Props) => {
   const contentComponent = await getContentComponent(slug);
 
   return (
-    <SandpackProvider
-      template="react-ts"
-      customSetup={{ dependencies: contentComponent.dependencies }}
-      options={{
-        externalResources: [
-          "https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4",
-        ],
-      }}
-      files={{
-        "/App.tsx": contentComponent.previewCode,
-        "/styles.css": defaultPreviewStyles,
-        ...contentComponent.sourceCode,
-      }}
-    >
+    <ContentProvider contentComponent={contentComponent}>
       <main className="relative flex h-[calc(100dvh-theme(spacing.16))] justify-center">
         <ContentRenderer contentComponent={contentComponent} />
         <ResponsiveAside contentComponent={contentComponent} />
       </main>
-    </SandpackProvider>
+    </ContentProvider>
   );
 };
-
-const defaultPreviewStyles = `
-* {
-  box-sizing: border-box;
-}
-
-html,
-body,
-#root {
-  width: 100%;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
-`;
 
 export default Page;
