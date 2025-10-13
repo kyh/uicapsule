e34066931310b1e2f4dc3241e8c0628c37d2aac5
Kaiyu Hsu <im.kaiyu@gmail.com>
2025-10-12 21:36:54 -0700

    use libsql

--- packages/db/src/drizzle-client.ts
+++ packages/db/src/drizzle-client.ts
@@ -1,18 +1,18 @@
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
-import Database from "better-sqlite3";
-import { drizzle } from "drizzle-orm/better-sqlite3";
+import { drizzle } from "drizzle-orm/libsql";
 
 import * as schema from "./drizzle-schema";
 import * as schemaAuth from "./drizzle-schema-auth";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const defaultDatabasePath = resolve(__dirname, "sqlite.db");
-const sqlite = new Database(process.env.DATABASE_URL ?? defaultDatabasePath);
 
-export const db = drizzle({
-  client: sqlite,
-  schema: { ...schema, ...schemaAuth },
-});
+export const db = drizzle(
+  process.env.DATABASE_URL ?? `file:${defaultDatabasePath}`,
+  {
+    schema: { ...schema, ...schemaAuth },
+  },
+);
 
 export type Db = typeof db;
