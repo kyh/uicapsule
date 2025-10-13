b9aa7b5e742c1b526a10e368e4c80d93eae685b3
Kaiyu Hsu <im.kaiyu@gmail.com>
2025-10-12 22:30:38 -0700

    use web library

--- packages/db/src/drizzle-client.ts
+++ packages/db/src/drizzle-client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql/node";
+import { drizzle } from "drizzle-orm/libsql/web";
 
 import * as schema from "./drizzle-schema";
 import * as schemaAuth from "./drizzle-schema-auth";
