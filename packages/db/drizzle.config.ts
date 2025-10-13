307ca51075a4bc687223c04e934b5d3febd65400
Kaiyu Hsu <im.kaiyu@gmail.com>
2025-10-12 22:10:57 -0700

    use turso

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,14 +1,12 @@
 import { resolve } from "node:path";
 import type { Config } from "drizzle-kit";
 
-const defaultDatabaseFile = resolve(process.cwd(), "src", "sqlite.db");
-const databaseUrl = process.env.DATABASE_URL ?? defaultDatabaseFile;
-
 export default {
-  dialect: "sqlite",
+  dialect: "turso",
   schema: ["./src/drizzle-schema-auth.ts", "./src/drizzle-schema.ts"],
   out: "./drizzle",
   dbCredentials: {
-    url: databaseUrl,
+    url: process.env.TURSO_DATABASE_URL ?? "",
+    authToken: process.env.TURSO_AUTH_TOKEN ?? "",
   },
 } satisfies Config;
--- packages/db/src/drizzle-client.ts
+++ packages/db/src/drizzle-client.ts
@@ -1,18 +1,17 @@
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
+import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 
 import * as schema from "./drizzle-schema";
 import * as schemaAuth from "./drizzle-schema-auth";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const defaultDatabasePath = resolve(__dirname, "sqlite.db");
+const client = createClient({
+  url: process.env.TURSO_DATABASE_URL ?? "",
+  authToken: process.env.TURSO_AUTH_TOKEN ?? "",
+});
 
-export const db = drizzle(
-  process.env.DATABASE_URL ?? `file:${defaultDatabasePath}`,
-  {
-    schema: { ...schema, ...schemaAuth },
-  },
-);
+export const db = drizzle({
+  client,
+  schema: { ...schema, ...schemaAuth },
+});
 
 export type Db = typeof db;
