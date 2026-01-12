5ba1595dc066842f3563a3b72743dfa5509f9168
Kaiyu Hsu <im.kaiyu@gmail.com>
2026-01-12 12:39:18 -0800

    add sync workflow

--- content/spinner-pixel-grid/preview.tsx
+++ content/spinner-pixel-grid/preview.tsx
@@ -6,7 +6,7 @@ const sizes = [6, 10, 14];
 
 const Preview = () => {
   return (
-    <div className="grid min-h-screen grid-cols-2 bg-black sm:grid-cols-3 md:grid-cols-4">
+    <div className="grid min-h-screen grid-cols-3 bg-black pb-32">
       {spinnerVariants.map((variant) =>
         sizes.map((size) => (
           <div
@@ -14,7 +14,9 @@ const Preview = () => {
             className="flex flex-col items-center justify-center gap-3 border border-white/10 p-6"
           >
             <SpinnerPixelGrid variant={variant} size={size} gridSize={5} />
-            <span className="text-xs text-gray-500">{variant}</span>
+            <span className="text-xs text-gray-500">
+              {variant} ({size}px)
+            </span>
           </div>
         )),
       )}
