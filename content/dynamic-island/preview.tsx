c9a0201d298f6552c51a867cb6e0818885d65624
Kaiyu Hsu <im.kaiyu@gmail.com>
2026-05-02 23:18:56 -0700

    Add dynamic island

---
+++ content/dynamic-island/preview.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import DynamicIsland from "./dynamic-island";
+
+const Preview = () => {
+  return (
+    <main className="flex h-dvh items-center justify-center overflow-hidden bg-[radial-gradient(circle_at_50%_30%,#334155_0%,#0f172a_42%,#020617_100%)] p-5">
+      <div className="w-full max-w-[420px]">
+        <DynamicIsland />
+      </div>
+    </main>
+  );
+};
+
+export default Preview;
